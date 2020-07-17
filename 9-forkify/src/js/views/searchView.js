b180d33fbc497b5ac4a92d764d527f7e1f98bf3e
David <hsienwu830808@gmail.com>
2020-07-17 23:44:45 +0800

    wip: render buttons and adjust render results

--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -49,6 +49,22 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup)
 }
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe)
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage)
+
+    if (page === 1 && pages > 1) {
+        // Only button to go to next page
+    }  else if (page < pages) {
+        // Both buttons
+    }
+    else if (page === pages && pages > 1) {
+        // Only button to go to prev page
+
+    }
+}
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page - 1) * resPerPage
+    const end = page * resPerPage
+    recipes.slice(start, end).forEach(renderRecipe)
 }
