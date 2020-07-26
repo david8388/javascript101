b94cd58ae44726c5f5abe500500c1710469f6b39
David <hsienwu830808@gmail.com>
2020-07-26 08:45:32 +0800

    feat: update recipe servings

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -94,3 +94,19 @@ const controlRecipe = async () => {
 }
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            recipeView.updateServingsIngredients(state.recipe)
+            state.recipe.updateServings('dec')
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc')
+        recipeView.updateServingsIngredients(state.recipe)
+    }
+    console.log(state.recipe)
+})
