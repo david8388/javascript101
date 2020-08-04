202119f2f928e9f62a8542c5d3caf3e43b8a354d
David <hsienwu830808@gmail.com>
2020-08-04 20:28:43 +0800

    feat: implementing persistent data with localStorage

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -137,10 +137,6 @@ elements.shopping.addEventListener('click', e => {
 /**
  * Like controller
  */
-// Testing 
-state.likes = new Likes()
-likesView.toggleLikeMenu(state.likes.getNumLikes())
-
 const controllerLike = () => {
     if (!state.likes) state.likes = new Likes()
     const currentID = state.recipe.id
@@ -170,6 +166,21 @@ const controllerLike = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes())
 }
+
+// Restore liked recipes on page load
+window.addEventListener('load', ()=> {
+    // Testing 
+    state.likes = new Likes()
+
+    // Restore likes
+    state.likes.readStorage()
+    
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes())
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like))
+})
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
